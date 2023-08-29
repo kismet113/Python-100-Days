9e1b39fe488810312ad6148e7b1f49a9fc5f9af5
kismet13 <96873905+kismet113@users.noreply.github.com>
2023-08-29 21:21:45 +0800

    Update piecewise.py

--- Day01-15/code/Day03/piecewise.py
+++ Day01-15/code/Day03/piecewise.py
@@ -17,3 +17,13 @@ elif x >= -1:
 else:
     y = 5 * x + 3
 print('f(%.2f) = %.2f' % (x, y))
+
+# way/method 2
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+elif x >= -1:
+    y = x + 2
+else:
+    y = 5 * x + 3
+print(f'f({x}) = {y}')
